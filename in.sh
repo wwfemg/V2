e8e4d25dd3eda03e0f26f7aa764aacfd5164d894
wwfemg <31567222+wwfemg@users.noreply.github.com>
2023-08-26 10:13:40 +0800

    Update in.sh

--- in.sh
+++ in.sh
@@ -1,27 +1,59 @@
+#!/bin/bash
 
-NaiveProxy
+# 函數用於重試失敗的命令
+retry_command() {
+  for i in {1..5}; do
+    "$@" && return 0
+    echo "Command failed. Retrying in 5 seconds..."
+    sleep 5
+  done
+  echo "Command failed after 5 attempts. Exiting."
+  exit 1
+}
 
-1、apt update
-2、apt install vim
-3、apt install -y debian-keyring debian-archive-keyring apt-transport-https
-4、curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-5、curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
-6、apt update
-7、apt install caddy
-9、進入：cd /etc/caddy/
-10、編輯：vim Caddyfile   
+# 更新和安裝必要軟體
+retry_command apt update -y
+retry_command apt install -y curl wget socat
 
-更換端口為x-ui面板端口和去掉#
+# 設定BBR
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+retry_command sysctl -p
+retry_command lsmod | grep bbr
 
-11、systemctl reload caddy
-12、apt  install golang-go
-13、apt remove golang-go
-14、wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
-15、rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
-16、export PATH=$PATH:/usr/local/go/bin
-17、go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
-18、~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
-19、輸入ls 看到caddy
-20、mv caddy /usr/bin/
-21、vim /etc/caddy/Caddyfile
-22、改配置文件
+# 安裝x-ui，並自動輸入用戶名、密碼和端口
+echo -e "naive\nnaive\n8400" | retry_command bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+
+# 更新和安裝Caddy
+retry_command apt update
+retry_command apt install -y vim curl debian-keyring debian-archive-keyring apt-transport-https
+retry_command curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+retry_command curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+retry_command apt update
+retry_command apt install -y caddy
+
+# 更改Caddyfile
+cd /etc/caddy/
+sed -i 's/8080/8400/g' Caddyfile
+sed -i 's/# //g' Caddyfile
+
+# 重新加載Caddy
+retry_command systemctl reload caddy
+
+# 其他步驟
+retry_command apt install -y golang-go
+retry_command apt remove -y golang-go
+
+# 根據架構安裝Go
+if [ "$(uname -m)" == "x86_64" ]; then
+  retry_command wget https://go.dev/dl/go1.20.7.linux-amd64.tar.gz
+  retry_command rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.7.linux-amd64.tar.gz
+else
+  retry_command wget https://go.dev/dl/go1.21.0.linux-arm64.tar.gz
+  retry_command rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-arm64.tar.gz
+fi
+
+export PATH=$PATH:/usr/local/go/bin
+retry_command go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+retry_command ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
+retry_command mv caddy /usr/bin/
