3764c3234a3e8a6a06680cb10fcfca6ee8a11797
wwfemg <31567222+wwfemg@users.noreply.github.com>
2023-08-28 00:42:32 +0800

    Update in.sh

--- in.sh
+++ in.sh
@@ -25,7 +25,7 @@ echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
 lsmod | grep bbr
 
-# 安裝x-ui，不自動設定用戶名、密碼和端口
+# 安裝x-ui
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 
 # 更新和安裝Caddy
