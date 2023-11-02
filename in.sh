14aa05487442cd5618533837e7df0362aab667d2
wwfemg <31567222+wwfemg@users.noreply.github.com>
2023-11-02 16:17:06 +0800

    Update in.sh

--- in.sh
+++ in.sh
@@ -74,11 +74,11 @@ if ! check_step_done "install_go"; then
     apt install -y golang-go || error_exit
     apt remove -y golang-go || error_exit
     if [ "$(uname -m)" == "x86_64" ]; then
-      wget https://go.dev/dl/go1.20.7.linux-amd64.tar.gz || error_exit
-      rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.7.linux-amd64.tar.gz || error_exit
+      wget https://go.dev/dl/go1.21.2.linux-amd64.tar.gz || error_exit
+      rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.2.linux-amd64.tar.gz || error_exit
     else
-      wget https://go.dev/dl/go1.21.0.linux-arm64.tar.gz || error_exit
-      rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-arm64.tar.gz || error_exit
+      wget https://go.dev/dl/go1.21.2.linux-arm64.tar.gz || error_exit
+      rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.2.linux-arm64.tar.gz || error_exit
     fi
     echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
     source ~/.bashrc
